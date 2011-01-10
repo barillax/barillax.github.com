0ae90e4184cfb111ed6bc85b21cbd74329217585
Ori Ratner <ratner@gmail.com>
2011-01-10 00:21:27 -0500

    added footer! all that remains is fixing single layout to be dry, and well, content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   $('#topnav').localScroll();
+
+  // slick html5 custom data trick for image rollover from http://www.selfcontained.us/2008/03/08/simple-jquery-image-rollover-script/
+  $(function() {
+      $('img[data-hover]').hover(function() {
+          $(this).attr('tmp', $(this).attr('src')).attr('src', $(this).attr('data-hover')).attr('data-hover', $(this).attr('tmp')).removeAttr('tmp');
+      }).each(function() {
+          $('<img />').attr('src', $(this).attr('data-hover'));
+      });;
+  });
+  
+  // make all links open in new tab
+  $("a").attr("target","_blank");
 })
